5aeae2941988c0fc903e230eba85db11325263ee
Edwin Andermyr <andermyredwin@gmail.com>
2023-12-02 16:57:30 +0100

    First pompt initialized

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").textContent = "[ - a Film Game - ]"
+let directorChoice = prompt(" ~~~ INSTRUCTIONS ~~~ \n" +
+                          "[- Choose your Film Director - ]\n" +
+                          "  ~~~Choose Between ~~~ \n" +
+                          "[- Steven Spielberg -  ]\n" +
+                          "[- Ridley Scott - ]\n" +
+                          "[- Martin Scorsese - ]");
